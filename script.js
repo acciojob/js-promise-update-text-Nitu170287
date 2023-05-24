6e41cad5e7ab081b29d6fdf50fcaa4fa3d082fc0
Nitu170287 <108494260+Nitu170287@users.noreply.github.com>
2023-05-24 15:03:55 -0400

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const output = document.getElementById("output") ;
+
+let prom = new Promise((resolve, reject)=>{
+	setTimeout(()=>{
+	output.innerText = "Hello, world!" 	;
+		resolve();
+	},1000);
+})
